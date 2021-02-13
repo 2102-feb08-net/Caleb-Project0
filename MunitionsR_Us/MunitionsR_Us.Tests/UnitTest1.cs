e66fa0c9c10fd7cfa6e58b7a2421a897175250ed
Caleb Owens <owenscaleb75@gmail.com>
2021-02-12 19:50:38 -0600

    Creation of Project0, git and base project files

---
+++ MunitionsR_Us/MunitionsR_Us.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MunitionsR_Us.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ MunitionsR_Us/MunitionsR_Us/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MunitionsR_Us
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
