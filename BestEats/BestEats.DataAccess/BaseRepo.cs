d4cd402e40f040c1d383177465bd09d9cd15576f
Caleb Owens <owenscaleb75@gmail.com>
2021-02-22 16:28:31 -0600

    working on BaseRepo ICustomerRepo, Disposable

---
+++ BestEats/BestEats.DataAccess/BaseRepo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using BestEats;
+
+namespace BestEats.DataAccess
+{
+    public class BaseRepo : ICustomerRepo
+    {
+        private readonly DB_BestEatsContext _context;
+        
+        public BaseRepo(DB_BestEatsContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+       
+
+
+        ///<remark> Customer Related Repos </remark>
+        public IEnumerable<Customer> GetCustomers(string name = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        public Customer GetCustomerID(int CustomerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterCustomer(BestEats.Customer DBcustomer)
+        {
+            Customer customer = new Customer
+            {
+                FullName = DBcustomer.CustomerFullName,
+                CustPassword = DBcustomer.CustomerPassword
+            };
+
+            _context.Add(customer);
+        }
+
+        public void UnregisterCustomer(int customerID)
+        {
+            Customer customer = _context.Customers.Find(customerID);
+            _context.Remove(customer);
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+
+
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BestEats/BestEats.DataAccess/ICustomerRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+
+namespace BestEats.DataAccess
+{
+    public interface ICustomerRepo
+    {
+        IEnumerable<Customer> GetCustomers(string name = null);
+
+        Customer GetCustomerID(int CustomerID);
+
+        void RegisterCustomer(BestEats.Customer customer);
+
+        void UnregisterCustomer(int CustomerID);
+
+        void UpdateCustomer(Customer customer);
+
+        void Save();
+    }
+}
